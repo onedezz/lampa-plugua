3fb98fb7310df2337b48ae755da39e845cb7758f
onedezz <160807249+onedezz@users.noreply.github.com>
2026-07-29 06:03:17 +0300

    Refactor MAL plugin initialization and menu addition

--- mal.js
+++ mal.js
@@ -1,46 +1,41 @@
 (function () {
-    'use me strict';
+    'use strict';
 
-    // Функція ініціалізації плагіна
     function startPlugin() {
         if (window.plugin_mal_installed) return;
         window.plugin_mal_installed = true;
 
-        // Додаємо пункт в головне меню Lampa
+        // 1. Реєструємо компонент сторінки MAL
         Lampa.Component.add('mal_anime', function () {
             var self = this;
-            var object = {
-                title: 'Аніме (MAL)',
-                url: 'https://api.jikan.moe/v4/top/anime'
-            };
 
             this.create = function () {
+                var _this = this;
                 this.activity.loader(true);
 
                 // Запит до Jikan API (MyAnimeList)
                 $.ajax({
-                    url: object.url,
+                    url: 'https://api.jikan.moe/v4/top/anime',
                     type: 'GET',
                     dataType: 'json',
                     success: function (response) {
                         if (response && response.data) {
-                            var cards = self.formatCards(response.data);
-                            self.buildList(cards);
+                            var cards = _this.formatCards(response.data);
+                            _this.buildList(cards);
                         } else {
-                            self.empty();
+                            _this.empty();
                         }
                     },
                     error: function () {
-                        Lampa.Noty.show('Помилка завантаження даних з MyAnimeList');
-                        self.empty();
+                        Lampa.Noty.show('Помилка завантаження даних з MAL');
+                        _this.empty();
                     },
                     complete: function () {
-                        self.activity.loader(false);
+                        _this.activity.loader(false);
                     }
                 });
             };
 
-            // Перетворення даних MAL у формат картки Lampa
             this.formatCards = function (items) {
                 return items.map(function (item) {
                     return {
@@ -48,25 +43,23 @@
                         title: item.title_japanese || item.title,
                         original_title: item.title_english || item.title,
                         name: item.title,
-                        img: item.images.jpg.large_image_url || item.images.jpg.image_url,
-                        background_image: item.images.jpg.large_image_url,
+                        img: item.images && item.images.jpg ? item.images.jpg.large_image_url || item.images.jpg.image_url : '',
+                        background_image: item.images && item.images.jpg ? item.images.jpg.large_image_url : '',
                         vote_average: item.score || 0,
                         release_date: item.aired && item.aired.from ? item.aired.from.split('T')[0] : '',
                         first_air_date: item.aired && item.aired.from ? item.aired.from.split('T')[0] : '',
                         overview: item.synopsis || 'Опис відсутній.',
-                        type: 'tv', // Вказуємо тип для коректного пошуку онлайн-джерел в Lampa
+                        type: 'tv',
                         source: 'tmdb'
                     };
                 });
             };
 
-            // Відображення сітки з картками
             this.buildList = function (cards) {
                 var scroll = new Lampa.Scroll({ mask: true, over: true });
                 var items = new Lampa.Items({ items: cards });
 
-                items.on('click', function (card, element) {
-                    // При кліку відкриваємо стандартне вікно картки Lampa для пошуку балансерів (CUB, Rezka тощо)
+                items.on('click', function (card) {
                     Lampa.Activity.push({
                         url: '',
                         component: 'full',
@@ -87,10 +80,11 @@
             };
         });
 
-        // Додаємо кнопку плагіна в ліве меню
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') {
-                var menu_item = $('<li class="menu__item selector" data-action="mal_anime">' +
+        // 2. Додаємо пункт меню безпечним способом
+        function addMenuItem() {
+            var menu = $('.menu .menu__list');
+            if (menu.length && !menu.find('[data-action="mal_anime"]').length) {
+                var item = $('<li class="menu__item selector" data-action="mal_anime">' +
                     '<div class="menu__ico">' +
                         '<svg height="24" viewBox="0 0 24 24" width="24" fill="currentColor">' +
                             '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14.5v-9l6 4.5-6 4.5z"/>' +
@@ -99,23 +93,32 @@
                     '<div class="menu__text">Аніме (MAL)</div>' +
                 '</li>');
 
-                menu_item.on('hover:enter', function () {
+                item.on('hover:enter', function () {
                     Lampa.Activity.push({
                         title: 'Топ Аніме (MAL)',
                         component: 'mal_anime'
                     });
                 });
 
-                $('.menu .menu__list').append(menu_item);
+                menu.append(item);
+            }
+        }
+
+        // Пробуємо додати одразу або чекаємо на готовність меню
+        addMenuItem();
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type === 'ready') {
+                addMenuItem();
             }
         });
     }
 
+    // Запуск плагіна
     if (window.appready) {
         startPlugin();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') startPlugin();
+            if (e.type === 'ready') startPlugin();
         });
     }
 })();
