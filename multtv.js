359985bfa9af9840ed59ead4c3e7ee00d48cc96e
onedezz <160807249+onedezz@users.noreply.github.com>
2026-07-30 07:48:27 +0300

    Update plugin to use TVMaze API for animated shows

--- multtv.js
+++ multtv.js
@@ -2,119 +2,70 @@
     'use strict';
 
     /**
-     * EXTERNAL API ANIMATED TV SHOWS PLUGIN FOR LAMPA
-     * Demonstrates fetching from an external API and mapping to TMDB objects.
+     * REAL EXTERNAL API ANIMATED TV SHOWS (Powered by TVMaze)
      */
 
     var API_CONFIG = {
-        title: 'Мультсеріали (External API)',
-        icon: '<svg viewBox="0 0 24 24" fill="#FFC107" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z"/><path d="M9.5 7.5v7l5.5-3.5z" fill="#ffffff"/></svg>',
-        
-        // Приклад ендпоінтів стороннього API (Trakt / TVMaze / Custom API)
-        categories: [
-            { title: "🔥 Трендові мультсеріали", type: "trending" },
-            { title: "⭐ Найвищий рейтинг спільноти", type: "popular" },
-            { title: "⚡ Найдешевші свіжі новинки", type: "recent" }
-        ]
+        title: 'Мультсеріали (TVMaze API)',
+        icon: '<svg viewBox="0 0 24 24" fill="#FFC107" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z"/><path d="M9.5 7.5v7l5.5-3.5z" fill="#ffffff"/></svg>'
     };
 
-    // 1. Хелпер для конвертації об'єкта стороннього API у формат Lampa TMDB
-    function mapExternalToLampa(externalItem) {
-        // Якщо API повертає TMDB ID напряму (наприклад, Trakt API):
-        var tmdbId = externalItem.ids ? externalItem.ids.tmdb : externalItem.tmdb_id;
-
+    function mapTvMazeToLampa(item) {
         return {
-            id: tmdbId, // Головний TMDB ID для сумісності з плеєрами та торентами
-            name: externalItem.title || externalItem.name,
-            original_name: externalItem.original_title || externalItem.original_name,
-            overview: externalItem.overview || externalItem.summary || '',
-            poster_path: externalItem.poster_path ? (externalItem.poster_path.startsWith('http') ? externalItem.poster_path : externalItem.poster_path) : null,
-            vote_average: externalItem.rating || externalItem.vote_average || 0,
-            first_air_date: externalItem.first_air_date || externalItem.year ? externalItem.year.toString() : '',
-            method: 'tv' // Вказуємо Lampa, що це серіал
+            id: item.id,
+            imdb_id: item.externals ? item.externals.imdb : null, // Завдяки IMDb ID балансери Lampa знайдуть відео!
+            name: item.name,
+            original_name: item.name,
+            overview: item.summary ? item.summary.replace(/<[^>]*>?/gm, '') : '',
+            poster_path: item.image ? item.image.original || item.image.medium : null,
+            vote_average: item.rating && item.rating.average ? item.rating.average : 0,
+            first_air_date: item.premiered || '',
+            method: 'tv'
         };
     }
 
-    // 2. Пошуковий фолбек (якщо стороннє API не має готового tmdb_id)
-    function resolveTmdbId(item, callback) {
-        if (item.id) return callback(item); // TMDB ID вже є
-
-        var searchUrl = Lampa.TMDB.api('search/tv?api_key=' + Lampa.TMDB.key() + '&query=' + encodeURIComponent(item.name));
-        var network = new Lampa.Reguest();
-        
-        network.silent(searchUrl, function (json) {
-            if (json && json.results && json.results.length) {
-                item.id = json.results[0].id; // Прив'язуємо знайдений TMDB ID
-                if (!item.poster_path) item.poster_path = json.results[0].poster_path;
-            }
-            callback(item);
-        }, function () {
-            callback(item);
-        });
-    }
-
-    // 3. Компонент відображення списків
-    function ExternalAnimatedTvMain(object) {
+    function TvMazeAnimatedTvMain(object) {
         var comp = new Lampa.InteractionMain(object);
 
         comp.create = function () {
             var _this = this;
             this.activity.loader(true);
-            var categories = API_CONFIG.categories;
-            var status = new Lampa.Status(categories.length);
+            var network = new Lampa.Reguest();
 
-            status.onComplite = function () {
-                var fulldata = [];
-                Object.keys(status.data).sort(function (a, b) { return a - b; }).forEach(function (key) {
-                    var data = status.data[key];
-                    if (data && data.results && data.results.length) {
-                        var cat = categories[parseInt(key)];
-                        Lampa.Utils.extendItemsParams(data.results, { style: { name: 'wide' } });
-                        fulldata.push({
-                            title: cat.title,
-                            results: data.results
-                        });
-                    }
-                });
+            // Справжнє працююче API TVMaze
+            var url = 'https://api.tvmaze.com/shows?page=1';
 
-                if (fulldata.length) {
-                    _this.build(fulldata);
+            network.silent(url, function (shows) {
+                if (Array.isArray(shows)) {
+                    // Фільтруємо лише анімаційні серіали (Animation)
+                    var animated = shows.filter(function (show) {
+                        return show.genres && show.genres.indexOf('Animation') !== -1 && show.image;
+                    }).map(mapTvMazeToLampa);
+
+                    Lampa.Utils.extendItemsParams(animated, { style: { name: 'wide' } });
+
+                    _this.build([{
+                        title: '🔥 Популярні мультсеріали (з TVMaze)',
+                        results: animated
+                    }]);
                     _this.activity.loader(false);
                 } else {
                     _this.empty();
                 }
-            };
-
-            // Запит даних із зовнішнього API
-            categories.forEach(function (cat, index) {
-                // Приклад запиту до вашого стороннього API або Trakt.tv
-                var externalApiUrl = 'https://api.example.com/animated-series/' + cat.type;
-                var network = new Lampa.Reguest();
-
-                network.silent(externalApiUrl, function (response) {
-                    var rawList = response.items || response || [];
-                    
-                    // Мапимо тайтли під Lampa
-                    var formattedResults = rawList.map(mapExternalToLampa).filter(function(i) {
-                        return i.id && i.poster_path; // Залишаємо лише з валідними ID та постером
-                    });
-
-                    status.append(index.toString(), { results: formattedResults });
-                }, function () {
-                    status.error();
-                });
+            }, function () {
+                _this.empty();
             });
 
             return this.render();
         };
 
-        // 4. Клік по картці -> перехід у рідну картку Lampa за TMDB ID
+        // При кліці відкриваємо картку
         comp.onItemSelect = function (item) {
             Lampa.Activity.push({
-                url: '',
-                component: 'full', // Відкриває стандартний інтерфейс картки Lampa
-                id: item.id,       // TMDB ID
-                method: 'tv',      // Режим серіалу
+                component: 'full',
+                id: item.id,
+                imdb_id: item.imdb_id,
+                method: 'tv',
                 card: item
             });
         };
@@ -123,16 +74,16 @@
     }
 
     function startPlugin() {
-        if (window.plugin_external_animtv_ready) return;
-        window.plugin_external_animtv_ready = true;
+        if (window.plugin_tvmaze_anim_ready) return;
+        window.plugin_tvmaze_anim_ready = true;
 
-        Lampa.Component.add('external_animtv_main', ExternalAnimatedTvMain);
+        Lampa.Component.add('tvmaze_anim_main', TvMazeAnimatedTvMain);
 
         function addMenuButton() {
             var menu = $('.menu .menu__list').eq(0);
-            if (!menu.length || menu.find('.menu__item[data-action="external_animtv"]').length) return;
+            if (!menu.length || menu.find('.menu__item[data-action="tvmaze_anim"]').length) return;
 
-            var btn = $(`<li class="menu__item selector" data-action="external_animtv">
+            var btn = $(`<li class="menu__item selector" data-action="tvmaze_anim">
                 <div class="menu__ico">${API_CONFIG.icon}</div>
                 <div class="menu__text">${API_CONFIG.title}</div>
             </li>`);
@@ -140,7 +91,7 @@
             btn.on('hover:enter', function () {
                 Lampa.Activity.push({
                     title: API_CONFIG.title,
-                    component: 'external_animtv_main',
+                    component: 'tvmaze_anim_main',
                     page: 1
                 });
             });
@@ -157,5 +108,5 @@
         }
     }
 
-    if (!window.plugin_external_animtv_ready) startPlugin();
+    if (!window.plugin_tvmaze_anim_ready) startPlugin();
 })();
